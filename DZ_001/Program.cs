e44f4dcc91681c5fabc7a2f24ed24f015e0e4198
Alexander <nosorog-2010@yandex.ru>
2022-08-29 11:54:05 +0300

    Save DZ1

---
+++ DZ_001/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите трёхзначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number<100 || number>999)
+{
+    Console.Write("Это не трёхзначное число, введите новое значение");
+}
+else
+{
+    int a = number%10;
+    Console.Write("Последняя цифра данного числа это " +a);
+}
