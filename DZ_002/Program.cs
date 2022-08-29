1241427f8fb5a70721f0eafbc4392e6b221df8ff
Alexander <nosorog-2010@yandex.ru>
2022-08-29 14:25:17 +0300

    Save DZ2

---
+++ DZ_002/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее.
+
+// Console.Write("Введите первое число ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if(a>b)
+// {
+//     Console.Write("max = "+5); 
+// }
+// else
+// {
+//     Console.WriteLine(b);
+// }
+
+Console.Write("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число ");
+ int b = Convert.ToInt32(Console.ReadLine());
+
+ if(a>b)
+ {
+    int max = a;
+    Console.Write("max = " +max);
+ }
+ else
+ {
+    int max = b;
+    Console.Write("max = " +max);
+ }
+
+ 
+ 
