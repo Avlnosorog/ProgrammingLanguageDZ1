32fa403121a7380c87d228c271d88d266aa18e70
Alexander <nosorog-2010@yandex.ru>
2022-08-29 18:20:36 +0300

    Save DZ4

---
+++ DZ_004/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число для проверки ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 2 == 0)
+{
+   Console.WriteLine(number+ " является чётным");
+}
+else
+{
+    Console.WriteLine(number+" является не чётным");
+}
