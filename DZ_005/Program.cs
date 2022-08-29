d75964b0a8eeefcbe194dec1b98026400db39b31
Alexander <nosorog-2010@yandex.ru>
2022-08-29 22:05:03 +0300

    Save DZ5

---
+++ DZ_005/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int numberNeg = 1;
+while(numberNeg <= number)
+{
+if(numberNeg % 2 == 0)
+{
+   Console.WriteLine(numberNeg);
+   numberNeg++;
+}   
+else
+{
+    numberNeg++;
+}
+}
